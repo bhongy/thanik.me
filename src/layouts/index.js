dbc72c7e903aadca2fabd3810302de075b87c22c
Thanik Bhongbhibhat <email@thanik.me>
2018-01-06 17:09:05 -0800

    Use normalize.css to set base style

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
-import './index.css';
+import 'normalize.css';
 
 const Footer = () => (
   <footer>
