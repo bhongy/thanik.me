ecba678cfaa9dad31d9e165eee122e87f3cf82a6
Thanik Bhongbhibhat <email@thanik.me>
2018-01-07 09:23:32 -0800

    Update footer style

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -4,21 +4,29 @@ import Container from '../components/container';
 import s from './footer.module.css';
 
 const ref = {
+  twitter: 'https://twitter.com/bhongbhibhat',
+  github: 'https://github.com/bhongy',
   frantic: 'http://frantic.im/writing/',
   gatsby: 'https://github.com/gatsbyjs/gatsby#showcase',
 };
 
+// TODO: figure out "the Gatsby & Typography.js way"
+// to set typography style for a section - e.g. footer
+
+// TODO: figure out "the Gatsby way" to create external link
+
 const Footer = () => (
   <footer className={s.footer}>
     <Container>
-      <p>See what I'm upto on Twitter and Github.</p>
-      <p>
-        Site inspired by <a href={ref.frantic}>Alex Kotliarskyi's Website</a>.
-      </p>
-      <p>
-        Site built with <a href={ref.gatsby}>GatsbyJS</a>.
-      </p>
+      <p>See what I'm upto on <a href={ref.twitter}>Twitter</a> and <a href={ref.github}>Github</a>.</p>
     </Container>
+    <div className={s.credits}>
+      <Container>
+        Site inspired by <a href={ref.frantic}>Alex Kotliarskyi's Website</a>
+        {' • '}
+        Site built with <a href={ref.gatsby}>GatsbyJS</a>.
+      </Container>
+    </div>
   </footer>
 );
 
