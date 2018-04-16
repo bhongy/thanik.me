382d6d37b68b2e13814512332df75b7136a49796
Thanik Bhongbhibhat <thanikb@pinterest.com>
2018-04-15 21:07:38 -0700

    Setup Webpack Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 module.exports = {
   exportPathMap() {
     return {
       '/': { page: '/' },
     };
+  },
+
+  webpack(config, { isServer }) {
+    if (!isServer && process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          reportFilename: './bundles/client.html',
+          openAnalyzer: true,
+        })
+      );
+    }
+    return config;
   },
 };
